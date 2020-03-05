064dad211e144930078c2132e671354c36259cf7
mlazzell17@gmail.com <MicheSi>
2020-03-05 16:27:47 -0700

    created app with CRA

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Michelle Sirimanivong</h1>
+    </div>
+  );
+}
+
+export default App;

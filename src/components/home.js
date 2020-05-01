1183c23e2d42513c40f053d50425c510483e620e
mlazzell17@gmail.com <MicheSi>
2020-05-01 09:32:49 -0700

    changed colors on home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import elephants from '../images/elephants.png'
+import elephants from '../images/elephants_gray.png';
+import michelle from '../images/michelle_white.png';
 
 const Home = props => {
     return (
         <div className='homeDiv'>
             <h4>Web & Design</h4>
             <img className='elephantImg' src={elephants} alt='3 Elephants'/>
+            <img className='michelle' src={michelle} alt='Michelle Sirimanivong' />
             <nav>
                 <a href='/'>About</a>|
                 <a href='/'>Work</a>|

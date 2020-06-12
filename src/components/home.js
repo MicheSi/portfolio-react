eaa4f4154aa093acb4319d26b1353e0e19b7d6a1
mlazzell17@gmail.com <MicheSi>
2020-06-12 10:19:11 -0700

    changed logo to one img

--- src/components/home.js
+++ src/components/home.js
@@ -2,13 +2,15 @@ import React from 'react';
 import NavBar from './nav';
 import elephants from '../images/elephants_gray.png';
 import michelle from '../images/michelle_white.png';
+import logo from '../images/logo.png'
 
 const Home = props => {
     return (
         <div className='homeDiv'>
             <h4>Web Developer & Designer</h4>
-            <img className='elephantImg' src={elephants} alt='3 Elephants'/>
-            <img className='michelle' src={michelle} alt='Michelle Sirimanivong' />
+            <img className='logo' src={logo} alt='logo'/>
+            {/* <img className='elephantImg' src={elephants} alt='3 Elephants'/>
+            <img className='michelle' src={michelle} alt='Michelle Sirimanivong' /> */}
            <NavBar />
         </div>
     )

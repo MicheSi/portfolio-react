140da92a394e7b8a82216384e01e211ccc11b48d
mlazzell17@gmail.com <MicheSi>
2020-06-12 10:25:06 -0700

    changed logo to pixels

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import NavBar from './nav';
-import elephants from '../images/elephants_gray.png';
-import michelle from '../images/michelle_white.png';
 import logo from '../images/logo.png'
 
 const Home = props => {
@@ -9,8 +7,6 @@ const Home = props => {
         <div className='homeDiv'>
             <h4>Web Developer & Designer</h4>
             <img className='logo' src={logo} alt='logo'/>
-            {/* <img className='elephantImg' src={elephants} alt='3 Elephants'/>
-            <img className='michelle' src={michelle} alt='Michelle Sirimanivong' /> */}
            <NavBar />
         </div>
     )

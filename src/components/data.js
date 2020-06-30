e870c057461b5759260464f3998076f2f0b34b5c
mlazzell17@gmail.com <MicheSi>
2020-06-30 11:45:29 -0700

    adding project data show on hover

---
+++ src/components/data.js
@@ -0,0 +1,22 @@
+const projectData = [
+    {
+        id: 1,
+        name: 'Data Driven Transit',
+        about: 'Built during Labs in Lambda School with a team of seven other students. The front end app was built using React and Redux with Styled Components and Reactstrap as syling libraries. I collaborated with a team of three Web students to build the app.'
+    },
+    {
+        id: 2,
+        name: 'Gigapet',
+        about: ''
+    },
+    {
+        id: 3,
+        name: 'Farm Fresh Produce',
+        about: ''
+    },
+    {
+        id: 4,
+        name: 'Nintendo Amiibos',
+        about: ''
+    },
+]

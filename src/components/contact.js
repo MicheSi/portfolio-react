a0f8d6ef7a05e106f43890dda766a00f3f10848d
mlazzell17@gmail.com <MicheSi>
2022-05-05 09:50:59 -0700

    updated email

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,29 +1,57 @@
-import React from 'react';
-import NavBar from './nav';
-import contact from '../images/contact.png';
+import React from "react";
+import NavBar from "./nav";
+import contact from "../images/contact.png";
 
-const Contact = props => {
-    return (
-        <section id='contact'>
-            <div className='titleDiv contactDiv'>
-                <img className='titleImg' src={contact} alt='Contact' />
-                <NavBar />
-            </div>
-            <div className='contactInfo'>
-                <div className='contactText'>
-                    <h2>Let's build something together!</h2>
-                    <p>If you'd like to collaborate on a project, please get in touch.</p>
-                </div>
-                <div className='iconLinks'>
-                    <a href='mailto:mlazzell17@gmail.com'><i className="fas fa-envelope-open-text"></i></a>
-                    <a href='https://www.linkedin.com/in/michellesirimanivong/' target='_blank' rel='noopener noreferrer'><i className="fab fa-linkedin"></i></a>
-                    <a href='https://github.com/MicheSi' target='_blank' rel='noopener noreferrer'><i className="fab fa-github"></i></a>
-                    <a href='https://twitter.com/msirimanivong' target='_blank' rel='noopener noreferrer'><i className="fab fa-twitter"></i></a>
-                    <a href='https://www.instagram.com/cookies.n.code/' target='_blank' rel='noopener noreferrer'><i className="fab fa-instagram-square"></i></a>               
-                </div>
-            </div>
-        </section>
-    )
-}
+const Contact = (props) => {
+  return (
+    <section id="contact">
+      <div className="titleDiv contactDiv">
+        <img className="titleImg" src={contact} alt="Contact" />
+        <NavBar />
+      </div>
+      <div className="contactInfo">
+        <div className="contactText">
+          <h2>Let's build something together!</h2>
+          <p>
+            If you'd like to collaborate on a project, please get in touch.
+          </p>
+        </div>
+        <div className="iconLinks">
+          <a href="mailto:contact@michellesirimanivong.com">
+            <i className="fas fa-envelope-open-text"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/michellesirimanivong/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-linkedin"></i>
+          </a>
+          <a
+            href="https://github.com/MicheSi"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-github"></i>
+          </a>
+          <a
+            href="https://twitter.com/msirimanivong"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a
+            href="https://www.instagram.com/cookies.n.code/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-instagram-square"></i>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Contact;
